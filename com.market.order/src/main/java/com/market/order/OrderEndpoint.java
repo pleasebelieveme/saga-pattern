613794bc8487fc65489f61edfd2434cddbb58539
believeme <cain1227@naver.com>
2025-11-21 12:18:26 +0900

    lesson: #11 payment 서버 RabbitMQ 에러 큐 구현 및 테스트 완료

--- com.market.order/src/main/java/com/market/order/OrderEndpoint.java
+++ com.market.order/src/main/java/com/market/order/OrderEndpoint.java
@@ -57,6 +57,7 @@ public class OrderEndpoint {
 		public DeliveryMessage toDeliveryMessage(UUID orderId){
 			return DeliveryMessage.builder()
 				.orderId(orderId)
+				.userId(userId)
 				.productId(productId)
 				.productQuantity(productQuantity)
 				.payAmount(payAmount)
